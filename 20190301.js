fc095e90224416a995182900bb901ea31dfbbb82
zhongwf <wenfeng.zhong@gmail.com>
2019-10-26 18:22:47 +0800

    Update 20190301.js

--- 20190301.js
+++ 20190301.js
@@ -1,5 +1,6 @@
 function FindProxyForURL(url, host) {
-	if(host.indexOf("thcic") >= 0 || host.indexOf("tsinghua") >= 0 || host.indexOf("166.111") >= 0 || host.indexOf("101.6") >= 0 || host.indexOf("182.92.156.85") >= 0 ||  host.indexOf("47.88.139.80") >= 0 ||  host.indexOf("192.168") >= 0 ){
+	//if(host.indexOf("thcic") >= 0 || host.indexOf("tsinghua") >= 0 || host.indexOf("166.111") >= 0 || host.indexOf("101.6") >= 0 || host.indexOf("182.92.156.85") >= 0 ||  host.indexOf("47.88.139.80") >= 0 ||  host.indexOf("192.168") >= 0 ){
+	if(host.indexOf("thcic") >= 0 || host.indexOf("tsinghua") >= 0 || host.indexOf("192.168") >= 0 ){
 		return "DIRECT";
 	}else{
 		//return "PROXY 172.16.11.129:1081";
